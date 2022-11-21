9918f1e234e069eb07228f30c87fec013e0926d3
Praneet_ Palak (papraneet@deloitte.com) <papraneet@your.hostname.com>
2022-11-21 15:47:56 +0530

    getSubscriptionByUserID

--- src/main/java/com/subscribe/mainp/repository/subscriptionrepo.java
+++ src/main/java/com/subscribe/mainp/repository/subscriptionrepo.java
@@ -1,9 +1,14 @@
 package com.subscribe.mainp.repository;
 
+import com.subscribe.mainp.entity.History;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.subscribe.mainp.entity.Subscription;
 
+import java.util.List;
+
 public interface subscriptionrepo extends JpaRepository<Subscription, Long>{
+    public List<Subscription> findByUserId(int userid);
+
 
 }
